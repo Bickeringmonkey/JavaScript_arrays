bc8e4a8cf7e7e7242f4ce888ab4dd97fe660a8da
Bickeringmonkey <smeadows110280@gmail.com>
2019-12-01 10:20:38 +0000

    first commit

---
+++ js/playlist.js
@@ -0,0 +1,7 @@
+var playList = [];
+playList.push('I did it my way', 'You only live twice');
+playList.unshift('Barbaras Back', 'spiderman', 'Time of my life', 'Bad');
+//playList.pop(); -- To take away from the end
+//playList.shift(); -- to take away from the beginning
+
+printList( playList );
